4401b8e8b7689a2fa24a3cbb6a8e4eaa6d7d7189
藤島太智 <4ts0t0i7c@gmail.com>
2021-03-22 21:39:21 +0900

    2021/3/22

---
+++ calcu.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div class="wrap">
+        <div class="screen">
+
+        </div>
+        <div class="line1">
+            <button class="ac"></button>
+            <button class="plm"></button>
+            <button class="per"></button>
+            <button class="div"></button>
+        </div>
+        <div class="line2">
+            <button class="one"></button>
+            <button class="two"></button>
+            <button class="three"></button>
+            <button class="mul"></button>
+        </div>
+        <div class="line3">
+            <button class="four"></button>
+            <button class="five"></button>
+            <button class="six"></button>
+            <button class="add"></button>
+        </div>
+        <div class="line4">
+            <button class="seven"></button>
+            <button class="eight"></button>
+            <button class="nine"></button>
+            <button class="sub"></button>
+        </div>
+        <div class="line5">
+            <button class="zero"></button>
+            <button class="dp"></button>
+            <button class="equal"></button>
+        </div>
+    </div>
+</body>
+</html>
