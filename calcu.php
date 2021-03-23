1128b1e91eaeac2d454bfe35c84702fb03aec02f
藤島太智 <4ts0t0i7c@gmail.com>
2021-03-23 22:02:48 +0900

    表示

--- calcu.php
+++ calcu.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge,chrome=1">
+    <title>calculator</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     <div class="wrap">
@@ -12,33 +13,33 @@
 
         </div>
         <div class="line1">
-            <button class="ac"></button>
-            <button class="plm"></button>
-            <button class="per"></button>
-            <button class="div"></button>
+            <button class="ac" type="submit" value="AC"></button>
+            <button class="plm" type="submit" value="+/-"></button>
+            <button class="per" type="submit" value="%"></button>
+            <button class="div" type="submit" value="÷"></button>
         </div>
         <div class="line2">
-            <button class="one"></button>
-            <button class="two"></button>
-            <button class="three"></button>
-            <button class="mul"></button>
+            <button class="one" type="submit" value="1"></button>
+            <button class="two" type="submit" value="2"></button>
+            <button class="three" type="submit" value="3"></button>
+            <button class="mul" type="submit" value="×"></button>
         </div>
         <div class="line3">
-            <button class="four"></button>
-            <button class="five"></button>
-            <button class="six"></button>
-            <button class="add"></button>
+            <button class="four" type="submit" value="4"></button>
+            <button class="five" type="submit" value="5"></button>
+            <button class="six" type="submit" value="6"></button>
+            <button class="add" type="submit" value="+"></button>
         </div>
         <div class="line4">
-            <button class="seven"></button>
-            <button class="eight"></button>
-            <button class="nine"></button>
-            <button class="sub"></button>
+            <button class="seven" type="submit" value="7"></button>
+            <button class="eight" type="submit" value="8"></button>
+            <button class="nine" type="submit" value="9"></button>
+            <button class="sub" type="submit" value="-"></button>
         </div>
         <div class="line5">
-            <button class="zero"></button>
-            <button class="dp"></button>
-            <button class="equal"></button>
+            <button class="zero" type="submit" value="0"></button>
+            <button class="dp" type="submit" value="."></button>
+            <button class="equal" type="submit" value="="></button>
         </div>
     </div>
 </body>
